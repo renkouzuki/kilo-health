de1036c3fa3c0b6feda22268f43deec7dd424e2c
renkouzuki <renkouzuki815@gmail.com>
2024-09-23 16:19:45 +0700

    changes;

--- database/migrations/0001_01_01_000000_create_roles_table.php
+++ database/migrations/0001_01_01_000000_create_roles_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name', 40);
             $table->timestamps();
         });
---
+++ database/migrations/2024_09_23_091600_create_header_slide_texts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('header_slide_texts', function (Blueprint $table) {
+            $table->id();
+            $table->text('slide_texts');
+            $table->foreignId('site_settings_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('header_slide_texts');
+    }
+};
