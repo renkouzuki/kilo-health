f1af5621513f45874ee985693b9fbb00aa79423d
renkouzuki <renkouzuki815@gmail.com>
2024-09-23 15:55:14 +0700

    changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
     }
     
     public function roles(){
-        return $this->hasMany(Role::class);
+        return $this->hasOne(Role::class);
     }
 
     public function posts(){
